97efe7f88c60e5de08b064f61cc25abf6b8dd062
Matheus Dutra <matheusbbdutra@gmail.com>
2024-11-08 21:30:19 -0300

    Refatorando e implementando Nuxt e Vuetify

---
+++ components/Navbar.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { User } from 'lucide-vue-next';
+
+export default {
+  components: {
+    User
+  }
+};
+</script>
+<template>
+  <v-navigation-drawer
+      expand-on-hover
+      rail
+  >
+  <v-list>
+    <v-list-item subtitle="sandra_a88@gmail.com" title="Sandra Adams">
+      <template v-slot:prepend>
+        <User class="mr-3"/>
+      </template>
+    </v-list-item>
+  </v-list>
+
+  <v-divider></v-divider>
+
+  <v-list density="compact" nav>
+    <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" href="/Dashboard"></v-list-item>
+    <v-list-item prepend-icon="mdi-bank" title="Contas" href="/Contas"></v-list-item>
+  </v-list>
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-list-item prepend-icon="mdi-logout" title="Logout" href="/Contas"></v-list-item>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
---
+++ components/forms/TransferenciaForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-form>
+    <v-text-field
+        v-model="descricao"
+        label="Descrição"
+        required
+    ></v-text-field>
+
+    <v-text-field
+        v-model="valor"
+        label="Valor"
+        prefix="R$"
+        required
+    ></v-text-field>
+    
+    <v-date-input
+        v-model="dataVencimento"
+        label="Data de Lançamento"
+        class="mt-4"
+        formatter="dd-MM-yyyy"
+    ></v-date-input>
+
+    <v-checkbox
+        v-model="efetivada"
+        label="Efetivada"
+    ></v-checkbox>
+    
+    <v-select
+        v-model="contaOrigem"
+        :items="contas"
+        label="Conta"
+        required
+        :menu-props="{ closeOnContentClick: false }"
+        :return-object="true"
+        item-text="name"
+        item-value="id"
+    ></v-select>
+
+    <v-select
+        v-model="contaDestino"
+        :items="contas"
+        label="Conta"
+        required
+        :menu-props="{ closeOnContentClick: false }"
+        :return-object="true"
+        item-text="name"
+        item-value="id"
+    ></v-select>
+
+    <!-- Botão Salvar -->
+    <v-btn color="primary" @click="submitForm">Salvar</v-btn>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import {useNuxtApp} from "#imports";
+
+const { $axios } = useNuxtApp();
+
+const descricao = ref('');
+const valor = ref('');
+const dataVencimento = ref<Date | null>(null);
+const efetivada = ref(false);
+
+const contaOrigem = ref(null);
+const contaDestino = ref(null);
+const contas = ref([]);
+
+const fetchContas = async () => {
+  contas.value = await $axios.get('/api/contas').then(res => res.data);
+};
+
+const submitForm = () => {
+
+};
+
+onMounted(() => {
+  fetchCategorias();
+  fetchSubcategorias();
+  fetchContas();
+});
+</script>
+
+<style scoped>
+</style>
